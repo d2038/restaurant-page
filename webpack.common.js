58899a6deae281fa20bcc90f95077e2c9aec9894
d2038 <35503394+d2038@users.noreply.github.com>
2023-07-20 12:29:02 -0400

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = env => {
+  return {
+    entry: {
+      index: './src/index.js',
+    },
+    output: {
+      filename: `[name]${env.WEBPACK_SERVE ? '' : '.[contenthash]'}.js`,
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        lang: 'en',
+        title: 'Arepa Bistro',
+        favicon: '',
+        template: './src/index.html',
+        filename: `[name]${env.WEBPACK_SERVE ? '' : '.[contenthash]'}.html`,
+      }),
+      new MiniCssExtractPlugin({
+        filename: `[name]${env.WEBPACK_SERVE ? '' : '.[contenthash]'}.css`,
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+  }
+};
