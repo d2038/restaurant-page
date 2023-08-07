d18b72e16d0da90917656293e30c4d42416b8125
d2038 <d2038.2011@gmail.com>
2023-08-07 17:42:57 -0400

    Add menu elements

---
+++ src/components/menu/menu.js
@@ -0,0 +1,38 @@
+import { createElement } from '../utils';
+import './menu.css';
+import reina from './reina.jpg';
+import pelua from './pelua.jpg';
+import domino from './domino.jpg';
+import perico from './perico.jpg';
+import llanera from './llanera.jpg';
+import patapata from './patapata.jpg';
+
+const menu = () => {
+    const cards = {
+        images: [reina, pelua, domino, perico, llanera, patapata],
+        titles: ['Reina Pepiada', 'La pelúa', 'Dominó', 'Perico', 'Llanera', 'Patapata'],
+        text: ['Arepa filled with shredded chicken, avocado, mayonnaise, and yellow cheese.',
+               'Arepa filled with shredded beef, yellow cheese, and tomato sauce.',
+               'Arepa filled with yellow cheese, ham, and bacon.',
+               'Arepa filled with scrambled eggs, onion, tomato, and cilantro.',
+               'Arepa filled with roast beef, yellow cheese, avocado, and tomato.',
+               'Arepa filled with yellow cheese, onion, and tomato.'
+              ]
+    };
+
+    const menu = createElement('div', 'menu');
+
+    for (let index = 0; index < cards.images.length; index++) {
+        const card = createElement('div', 'card');
+        const image = createElement('img', 'image', null, cards.images[index]);
+        const title = createElement('div', 'title', cards.titles[index]);
+        const text = createElement('div', 'text', cards.text[index]);
+
+        card.append(image, title, text);
+        menu.append(card);
+    }
+
+    return menu;
+};
+
+export default menu;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './preflight.css';
 import './style.css';
 import header from './components/header/header';
 import home from './components/home/home';
+import menu from './components/menu/menu';
 
 const app = document.querySelector('#content');
 app.appendChild(header());
-app.appendChild(home());
+app.appendChild(menu());
