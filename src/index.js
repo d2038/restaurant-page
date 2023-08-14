b2ff4f19ba39cf51b753a16cbe3500b8eda5457a
d2038 <35503394+d2038@users.noreply.github.com>
2023-08-14 17:36:54 -0400

    Add tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import './preflight.css';
 import './style.css';
-import header from './components/header/header';
 import home from './components/home/home';
 import menu from './components/menu/menu';
 import contact from './components/contact/contact';
 import initialPageLoad from './components/initialpageload';
 
-// const app = document.querySelector('#content');
-// app.appendChild(header());
-// app.appendChild(contact());
-
 initialPageLoad();
+
+const app = document.querySelector('#content');
+const tabs = {
+    home: document.querySelector('.nav-home'),
+    menu: document.querySelector('.nav-menu'),
+    contact: document.querySelector('.nav-contact')
+};
+
+// Add event listeners to tabs
+for (const tab in tabs) {
+    if (Object.hasOwnProperty.call(tabs, tab)) {
+        const element = tabs[tab];
+        element.addEventListener('click', changeTab);
+    }
+}
+
+function changeTab(event) {
+    const currentTab = event.target;
+
+    // Remove 'selected' class from all tabs
+    for (const tab in tabs) {
+        if (Object.hasOwnProperty.call(tabs, tab)) {
+            const element = tabs[tab];
+            element.classList.remove('selected');
+        }
+    }
+
+    currentTab.classList.add('selected');
+
+    app.removeChild(app.lastChild);
+    const selection = currentTab.textContent.toLowerCase();
+
+    switch (selection) {
+        case 'home':
+            app.append(home());
+            break;
+        case 'menu':
+            app.append(menu());
+            break;
+        case 'contact':
+            app.append(contact());
+            break;
+    }
+}
